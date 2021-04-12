ade0fe58f23b1c7998e5e1bfe9bd42f735a76ac7
knmrftw <2669997@gmail.com>
2021-04-12 16:01:07 +0300

    .gitignore_global, _path, improved the user_delete_method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user.present?
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'Вы успешно залогинились'
+      redirect_to root_path, notice: 'Вы успешно залогинились'
     else
       flash.now.alert = 'Неправильный логин или пароль'
       render :new
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Вы вышли из системы'
+    redirect_to root_path, notice: 'Вы вышли из системы'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
+    session[:user_id] = nil
     redirect_to root_url
   end
 
