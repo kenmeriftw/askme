1f9afcadefdb319ff07520cb22c43e922baadadc
Boris Parusnikov <2669997@gmail.com>
2021-06-07 16:34:26 +0300

    safe navigation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @questions = Hashtag.find_by(name: params[:query].gsub('#', '')).questions
+    @questions = Hashtag.find_by(name: params[:query].gsub('#', ''))&.questions
   end
 end
