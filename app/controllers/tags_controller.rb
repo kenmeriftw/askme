b8c8ef4add601a30f26bef86546339c911a1b379
knmrftw <2669997@gmail.com>
2021-06-12 15:47:27 +0300

    raise is questions.empty?

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   def show 
       @questions = Hashtag.find_by!(name: params[:name]).questions
+      raise ActiveRecord::RecordNotFound if @questions.empty?
   end
 end
